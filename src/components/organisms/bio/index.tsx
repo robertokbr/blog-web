fdd0f19dc8794227ccfaefa184baa34475b686bb
robertokbr <robertojuniordev@gmail.com>
2023-11-23 16:28:03 -0300

    chore: remove non necessary things

--- src/components/organisms/bio/index.tsx
+++ src/components/organisms/bio/index.tsx
@@ -8,15 +8,13 @@ const linkStyle = { borderBottom: '2px #6272a4 dashed', color: '#6272a4' };
 export function Bio({hideLinks ,...data}: BioProps) {
   return (
     <Flex align="center" justify="center" direction="column" {...data}>
-      <Link href="/links">
-        <Avatar
-          size="xl"
-          name="Roberto Junior"
-          src="https://avatars.githubusercontent.com/u/60328400?v=4"
-          border="2px solid #6272a4"
-          mb="2"
-        />
-      </Link>
+      <Avatar
+        size="xl"
+        name="Roberto Junior"
+        src="https://avatars.githubusercontent.com/u/60328400?v=4"
+        border="2px solid #6272a4"
+        mb="2"
+      />
       {!hideLinks && (
         <HStack spacing="4" mt="4">
           <Link href="https://github.com/robertokbr" target="_blank" aProps={linkStyle}>
